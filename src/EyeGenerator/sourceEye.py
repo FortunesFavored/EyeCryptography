3271b2b2fefdb4cde2ccb7f715b75861c776de8a
FortunesFavorite <gregory.j.maj@gmail.com>
2023-09-15 20:37:05 -0500

    build out ascii+32 generation

--- src/EyeGenerator/sourceEye.py
+++ src/EyeGenerator/sourceEye.py
@@ -9,19 +9,19 @@ from .Messages.rawEyes8 import msg as RawMessage8
 from .Messages.rawEyes9 import msg as RawMessage9
 
 
-class SourceEyes:
-    RawMessage1 = RawMessage1
-    RawMessage2 = RawMessage2
-    RawMessage3 = RawMessage3
-    RawMessage4 = RawMessage4
-    RawMessage5 = RawMessage5
-    RawMessage6 = RawMessage6
-    RawMessage7 = RawMessage7
-    RawMessage8 = RawMessage8
-    RawMessage9 = RawMessage9
+class SourceEyes(object):
 
     def __init__(self) -> None:
-            pass
+        self.East1 = RawMessage1
+        self.West1 = RawMessage2
+        self.East2 = RawMessage3
+        self.West2 = RawMessage4
+        self.East3 = RawMessage5
+        self.West3 = RawMessage6
+        self.East4 = RawMessage7
+        self.West4 = RawMessage8
+        self.East5 = RawMessage9
+        pass
 
     def GenerateMessage(self, rawMsg: dict, direction: int = 1):
         completeMsg = []
@@ -53,19 +53,36 @@ class SourceEyes:
         [valueList.append(int(x)-1) for x in base7Value if int(x) > 0]
         return valueList
     
-    def reverseHexPairs(self, hexString: str):
-        pointer = 0
-        revHexString = ''
-        while pointer < len(hexString):
-            revHexString = hexString[pointer:pointer+2:] + revHexString
-            pointer += 2
-        return revHexString
+    def getEyeGroups(self, generatedMsg, readingOrder: list = [0,1,2]):
+        groups = []
+        for i in range(0, len(generatedMsg), 2):
+            tracker = 0
+            if generatedMsg[i] == []: break
+            while tracker < len(generatedMsg[i])-1:
+                #  add first eye
+                try:
+                    eyeSet1 = [generatedMsg[i][tracker],generatedMsg[i][tracker+1],generatedMsg[i+1][tracker]]
+                    groups.append([eyeSet1[readingOrder[0]],eyeSet1[readingOrder[1]],eyeSet1[readingOrder[2]]])
+                except:
+                    pass
+                #  add second eye
+                try:
+                    eyeSet2 = [generatedMsg[i+1][tracker+2],generatedMsg[i+1][tracker+1],generatedMsg[i][tracker+2]]
+                    groups.append([eyeSet2[readingOrder[0]],eyeSet2[readingOrder[1]],eyeSet2[readingOrder[2]]])
+                except:
+                    pass
+                tracker += 3
+        return groups
     
-    def reverseMsgHex(self, rawMsg: dict):
-        reversedMessageDict = {}
-        for pair in rawMsg.keys():
-            reversedMessageDict[pair] = {
-                'a': self.reverseHexPairs(rawMsg[pair]['a']),
-                'b': self.reverseHexPairs(rawMsg[pair]['b']),
-            }
-        return reversedMessageDict
+    def generateValueList(self, eyeGroup: list):
+        return [int(''.join([str(i) for i in q])) for q in eyeGroup]
+    
+    def generateBaseNList(self, eyeValueList: list, base: int = 5):
+        return [int(q, base=base) for q in eyeValueList]
+    
+    def generateAscii32List(self, eyeGroup: list):
+        return [chr(v+32) for v in eyeGroup]
+    
+
+
+    
